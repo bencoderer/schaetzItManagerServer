df63c7547484d3abdaf2c1cc711b300e88308c9c
Anonymous <thomas.krenn@intratec.at>
2016-08-22 23:09:20 +0000

    added operator initialization

---
+++ server/boot/init-models.js.js
@@ -0,0 +1,36 @@
+/*eslint-env node */
+var async = require('async');
+module.exports = function(app) {
+  //data sources
+  var mongodbsvr = app.dataSources.mongodbsvr;
+  
+  //create all models
+  async.parallel({
+    operators: async.apply(createOperators),
+    schaetzers: async.apply(createSchaetzers),
+  }, function(err, results) {
+    if (err) throw err;
+    
+      console.log('> models created sucessfully');
+    
+  });
+  //create operators
+  function createOperators(cb) {
+    mongodbsvr.automigrate('Operator', function(err) {
+      if (err) return cb(err);
+      var Operator = app.models.Operator;
+      Operator.create([
+        {operatorKey: 'OP1', name: 'Tablet 1'},
+        {operatorKey: 'OP2', name: 'Tablet 2'}
+      ], cb);
+    });
+  }
+  
+  //create schaetzer
+  function createSchaetzers(cb) {
+    mongodbsvr.automigrate('Schaetzer', function(err) {
+      if (err) return cb(err);
+    });
+  }
+
+};
--- server/boot/root.js
+++ server/boot/root.js
@@ -1,3 +1,4 @@
+/*eslint-env node */
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
