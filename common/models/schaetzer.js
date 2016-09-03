ef6e1c5fad3617465f3987cdfffd23495a8330b4
Anonymous <thomas.krenn@intratec.at>
2016-09-03 08:49:05 +0000

    Feature: new endpint 'notSyncedTo' for Schaetzers

--- common/models/schaetzer.js
+++ common/models/schaetzer.js
@@ -1,3 +1,31 @@
+/*eslint-env node */
 module.exports = function(Schaetzer) {
+
+	Schaetzer.notSyncedTo = function(opKey, cb) {
+	  var SyncModel = Schaetzer.app.models.SyncSchaetzerToOperator;
+
+	  SyncModel.find(
+	  {
+	  	 where: {and: [{operatorKey: {neq: opKey}
+	  	 ,include : {relation: "schaetzers"}
+	  }, {sentToOperatorDate: null}]}},
+	  function(err, syncArray) { 
+	    var result = [];
+	    if (syncArray !== null) {
+	    	result = syncArray.map(function(currentValue) {
+    		    return currentValue.schaetzer;
+	    	});
+	    }
+	    cb(err, result);
+	  });    
+	};
+
+    Schaetzer.remoteMethod(
+    	'notSyncedTo', 
+    	{
+    	accepts: {arg: 'opKey', type: 'string', required: true},
+        http: {path: '/notSyncedTo/:opKey', verb: 'get'},
+        returns: {arg: 'schaetzerList', type: 'Array', root:true}
+    	});			
 
 };
